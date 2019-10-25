47629ee6df18042df29f9ffa23fa612317abc56c
Andrey Ponamarchuk <ponamarchuk.andrey@icloud.com>
2019-10-25 18:27:29 +0500

    Initial Commit

---
+++ Translator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Translator
+//
+//  Created by Андрей Понамарчук on 25.10.2019.
+//  Copyright © 2019 Андрей Понамарчук. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
